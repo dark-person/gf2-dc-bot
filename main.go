9ba4ea3057da266b1eda48caef5da8707ad57c09
dark-person <66205094+dark-person@users.noreply.github.com>
2025-01-22 20:53:30 +0800

    Load config when program start

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/dark-person/gf2-dc-bot/internal/config"
 )
 
+// Get current time in opinionated formatted string.
+func currentTimeStr() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+var cfg *config.DiscordConfig
+
 func main() {
+	var err error
+
 	// Start up log
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "Starting up...")
+	fmt.Println(currentTimeStr(), "Starting up...")
+
+	// Load config
+	cfg, err = config.LoadYaml("config.yaml")
+	if err != nil {
+		panic(err) // Program will never run properly when config not loaded
+	}
+
+	fmt.Println(currentTimeStr(),
+		"Config loaded. Token: ", cfg.Token, "Channels: ", cfg.ChannelID)
 
 	// Init task
 	initDailyTask()
