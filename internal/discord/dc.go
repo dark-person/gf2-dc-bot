4335ae50628830503e056578012597413076c09a
dark-person <66205094+dark-person@users.noreply.github.com>
2025-01-22 20:47:58 +0800

    Move send message to discord pkg

--- internal/discord/dc.go
+++ internal/discord/dc.go
@@ -1,21 +1,19 @@
-package main
+package discord
 
 import (
 	"fmt"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/dark-person/gf2-dc-bot/internal/config"
 )
 
-// Token of discord bot. This token is required to send message to discord channel.
-var discordToken string
-
 // Send message to discord channel. Please note that discord token must be set before call this function.
-func sendDiscordMsg(channelID string, message string) error {
-	if discordToken == "" || channelID == "" {
+func SendDiscordMsg(cfg *config.DiscordConfig, message string) error {
+	if cfg.Token == "" || cfg.ChannelID == "" {
 		return fmt.Errorf("discord token or channel ID not set")
 	}
 
-	discordBot, err := discordgo.New("Bot " + discordToken)
+	discordBot, err := discordgo.New("Bot " + cfg.Token)
 	if err != nil {
 		return fmt.Errorf("failed to create discord bot: %v", err)
 	}
@@ -25,7 +23,7 @@ func sendDiscordMsg(channelID string, message string) error {
 		return fmt.Errorf("failed to open discord connection: %v", err)
 	}
 
-	_, err = discordBot.ChannelMessageSend(channelID, message)
+	_, err = discordBot.ChannelMessageSend(cfg.ChannelID, message)
 	if err != nil {
 		return fmt.Errorf("failed to send message to discord: %v", err)
 	}
