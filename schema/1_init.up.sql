0fec8ae8e9adf1e9140ae1fd5571f62b5feb832f
dark-person <66205094+dark-person@users.noreply.github.com>
2025-02-20 20:29:16 +0800

    Add table `calendar_cycled_event` for cycled event

--- schema/1_init.up.sql
+++ schema/1_init.up.sql
@@ -33,3 +33,42 @@ VALUES
     ('活動物資', '20250116', '20250205', 0),
     ('塵煙', '20250209', '20250215', 0),
     ('活動物資', '20250206', '20250226', 0);
+
+--  ========================================================
+CREATE TABLE `const_cycle_event` (
+    cycle_event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cycle_event_name VARCHAR(255) NOT NULL,
+    cycle_day INTEGER NOT NULL,
+    remind_before INTEGER NOT NULL
+);
+
+INSERT INTO
+    `const_cycle_event` (
+        cycle_event_id,
+        cycle_event_name,
+        cycle_day,
+        remind_before
+    )
+VALUES
+    (0, '', -1, 0),
+    (1, '擴編實練 10 階段', 14, 3),
+    (2, '兵棋推演自殺 20 場', 0, 6);
+
+--  ========================================================
+CREATE TABLE "calendar_cycled_event" (
+    "cycle_event_id" VARCHAR(255) NOT NULL,
+    "deadline_at" VARCHAR(255) NOT NULL,
+    "is_auto_calc" INTEGER NOT NULL DEFAULT 1,
+    FOREIGN KEY("cycle_event_id") REFERENCES "const_cycle_event"("cycle_event_id") ON UPDATE CASCADE,
+    PRIMARY KEY("cycle_event_id", "deadline_at")
+);
+
+INSERT INTO
+    "calendar_cycled_event" (
+        "cycle_event_id",
+        "deadline_at",
+        "is_auto_calc"
+    )
+VALUES
+    (1, 20250210, 0),
+    (2, 20250311, 0);
